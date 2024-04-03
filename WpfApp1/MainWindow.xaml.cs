5e137ca142e6cc481f3c330d5150ea82bdf47d41
vadim <vadim@DESKTOP-BPKCT2O>
2024-04-03 22:20:23 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public void NavigateToPage(string navigate)
+        {
+            switch (navigate)
+            {
+                case "Page1":
+                    MainFrame.Navigate(new Page1());
+                break;
+                case "Page2":
+                    MainFrame.Navigate(new Page2());
+                    break;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage("Page1");
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage("Page2");
+        }
+    }
+}
---
+++ WpfApp1/Page2.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow)Application.Current.MainWindow).ListBox.Items.Remove(((MainWindow)Application.Current.MainWindow).ListBox.SelectedItem);
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            string Edit = EditTextBox.Text;
+            ((MainWindow)Application.Current.MainWindow).ListBox.Items.Remove(((MainWindow)Application.Current.MainWindow).ListBox.SelectedItem);
+            if (!string.IsNullOrEmpty(Edit))
+            {
+                ((MainWindow)Application.Current.MainWindow).ListBox.Items.Add(Edit);
+                EditTextBox.Clear();
+            }
+        }
+    }
+}
